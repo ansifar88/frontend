f6c056b66d68c92d8f5bfbdb4c44db905b141e65
ansif-ar88 <125662944+ansif-ar88@users.noreply.github.com>
2023-11-06 18:08:16 +0530

    main update

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,13 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ChakraBaseProvider } from '@chakra-ui/react';
-const GoogleClientId = import.meta.env.VITE_GAUTHKEY;
+// const GoogleClientId = import.meta.env.VITE_GAUTHKEY;
 const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
     <PersistGate loading={null} persistor={persistor}>
     <ChakraBaseProvider >
-      <GoogleOAuthProvider clientId={ GoogleClientId }>
+      <GoogleOAuthProvider clientId="912896584809-52s8hkqead2tptvq8ifl95su44l4pmd1.apps.googleusercontent.com">
       <React.StrictMode>
         <QueryClientProvider client={queryClient}>
           <App/>
